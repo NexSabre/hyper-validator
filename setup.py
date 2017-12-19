3eae050507e56a78617870664359cf5ae3560f8d
Nex Sabre <tomaszmaciejewski@outlook.com>
2017-12-19 20:59:13 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="hyper-validator",
+    description="XML Validator for Python 2.7",
+    version="0.1",
+    url="https://github.com/NexSabre/hyper-validator"
+)
